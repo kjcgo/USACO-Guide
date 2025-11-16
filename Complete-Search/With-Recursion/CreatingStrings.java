896cdcbab013feded6942788a0d0f6ef2aef4831
Keesha Go <113406963+kjcgo@users.noreply.github.com>
2025-11-15 21:03:31 -0500

    Update CreatingStrings.java

--- Complete-Search/With-Recursion/CreatingStrings.java
+++ Complete-Search/With-Recursion/CreatingStrings.java
@@ -6,17 +6,25 @@ public class CreatingStrings {
 	
 	//how many of each letter appears
 	static int[] letters = new int[26];
+
+	//input
 	static String[] myString;
+
+	//if we've included this index yet
 	static boolean[] counted;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		myString = br.readLine().split("");
 		counted = new boolean[myString.length];
+
+		//get counts for each letter's appearances
 		for(String x : myString) {
 			letters[(int)x.charAt(0) - 97]++;			
 		}
-		
+
+		//Distinct # of permutations in a multiset
+		// n! / (n1! * n2! * ... * nk!) in which n1, n2, and n3 are counts of distinct repeated elements
 		int denom = 0;
 		for(int x : letters) {
 			if(x > 1) {
@@ -37,11 +45,14 @@ public class CreatingStrings {
 		if(s.length() == myString.length) {
 			System.out.println(s);
 		}
-		
+
+		//starting from a-z, build string
 		for(int i = 0; i < 26; i++) {
 			if(letters[i] > 0) {
 				letters[i]--;
 				solve(s + (char)(i + 'a'));
+
+				//reset for generating strings starting at the next viable letter
 				letters[i]++;
 			} 
 		}
